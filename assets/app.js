d6c36db3a93897f21a3fb76367da11268a07eb27
Alexis CAU <dragent@users.noreply.github.com>
2024-04-30 19:09:36 +0200

    Création de la page index

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,3 @@
-import './bootstrap.js';
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -7,4 +6,5 @@ import './bootstrap.js';
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import 'bootstrap';
+import './styles/app.scss';
